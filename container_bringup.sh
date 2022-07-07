385dd4e39f4404460e23f084abb3590647454bda
Lixun <zhanglx@utexas.edu>
2022-07-07 15:30:17 -0500

    Added script to download github repos (credential is provided as parameter)

---
+++ container_bringup.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+##
+## $1: username:token 
+##
+
+if [[ $# -lt 1 ]]; then
+    echo "Need to provide credentials to download github repos"
+    echo "./container_bringup.sh username:token"
+    exit
+fi
+
+cd ~/
+
+# setup emacs
+git clone https://$1@github.com/zhanglx13/emacs_settings.git
+mv emacs_settings .emacs.d
+
+# setup myscrips
+git clone https://$1@github.com/zhanglx13/llvm-project-mlir-dev-utils.git
+mv llvm-project-mlir-dev-utils myscripts
+
+# setup orgfiles
+git clone https://$1@github.com/zhanglx13/orgfiles.git
+
+# Download llvm-project-mlir
+git clone https://$1@github.com/ROCmSoftwarePlatform/llvm-project-mlir.git
+
+# Download MIOpen
+git clone https://$1@github.com/ROCmSoftwarePlatform/MIOpen.git
+

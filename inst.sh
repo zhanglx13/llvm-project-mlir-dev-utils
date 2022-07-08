835824920ba4b414b6e00413070c04d8bf6661d6
Lixun Zhang <lixun.zhang@amd.com>
2022-07-08 20:12:03 +0000

    update inst.sh

--- inst.sh
+++ inst.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-cmake -G Ninja -DBUILD_FAT_LIBMLIRMIOPEN=1 ../
+cd ~/llvm-project-mlir/build/
+cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_FAT_LIBMLIRMIOPEN=1 ../
 ninja libMLIRMIOpen
 cmake --install . --component libMLIRMIOpen --prefix ~/dummy

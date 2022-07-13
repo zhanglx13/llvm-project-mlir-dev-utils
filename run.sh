ec9e14fe85827961f9970fb14723f6799ab8cc5b
Lixun Zhang <lixun.zhang@amd.com>
2022-07-13 20:45:39 +0000

    Added miopen-gen configs for testing

--- run.sh
+++ run.sh
@@ -156,7 +156,16 @@ if [[ $callMiopenGen -eq 1 ]]; then
     #MIOPEN_GEN_CMD="--operation conv2d -t f16 --fil_layout kcyx --in_layout nchw --out_layout nkhw --batchsize 128 --in_channels 8 --in_h 8 --in_w 8 --out_channels 128 --fil_w 4 --fil_h 4 --dilation_h 1 --dilation_w 1 --conv_stride_h 1 --conv_stride_w 1 --padding_h 0 --padding_w 0 -p=false -x2"
 
     ## The following config failed in the nightly run 2022-07-12
-    MIOPEN_GEN_CMD="--operation conv2d -t f16 -p=false -fil_layout=gkyxc -in_layout=nhwgc -out_layout=nhwgk -batchsize=256 -groupsize=1 -in_channels=3 -out_channels=64 -in_h=230 -in_w=230 -fil_h=7 -fil_w=7 --dilation_h=1 --dilation_w=1 --padding_h=0 --padding_w=0 --conv_stride_h=2 --conv_stride_w=2 -pv -rand 1 --rand_type float --x2"
+    #MIOPEN_GEN_CMD="--operation conv2d -t f16 -p=false -fil_layout=gkyxc -in_layout=nhwgc -out_layout=nhwgk -batchsize=256 -groupsize=1 -in_channels=3 -out_channels=64 -in_h=230 -in_w=230 -fil_h=7 -fil_w=7 --dilation_h=1 --dilation_w=1 --padding_h=0 --padding_w=0 --conv_stride_h=2 --conv_stride_w=2 -pv -rand 1 --rand_type float --x2"
+
+    ## failed with threshold=15% from padding_kernel_gemmN.mlir line 6
+    ## 15% failed but 20% passed
+    MIOPEN_GEN_CMD="--operation conv2d_bwd_weight -t f16 -p=false -fil_layout=gkyxc -in_layout=nhwgc -out_layout=nhwgk -batchsize=64 -groupsize=1 -in_channels=3 -out_channels=64 -in_h=224 -in_w=224 -fil_h=7 -fil_w=7 --dilation_h=1 --dilation_w=1 --padding_h=3 --padding_w=3 --conv_stride_h=2 --conv_stride_w=2  -pv -rand 1 --rand_type float --x2"
+
+    ## failed with threshold=15% from conv2d_host_validation_f16_bwd.mlir line 71
+    ## even 20% failed
+    ## 25% seemed to work
+    #MIOPEN_GEN_CMD=" --operation=conv2d_bwd_weight -t f16 -fil_layout=kcyx -in_layout=nchw -out_layout=nkhw -batchsize=16 -in_channels=64 -out_channels=64 -in_h=14 -in_w=14 -fil_h=1 -fil_w=1 --dilation_h=1 --dilation_w=1 --padding_h=1 --padding_w=1 --conv_stride_h=1 --conv_stride_w=1 -p=false -pv -rand 1 --rand_type float -x2"
 
     ## last config in fwd_i8
     #MIOPEN_GEN_CMD="--operation conv2d -t i8 -x2 --fil_layout kcyx --in_layout nchw --out_layout nkhw --batchsize 256 --in_channels 64 --in_h 56 --in_w 56 --out_channels 64 --fil_h 3 --fil_w 3 --dilation_h 1 --dilation_w 1 --conv_stride_h 1 --conv_stride_w 1 --padding_h 1 --padding_w 1"

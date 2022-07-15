d28e2a1e59e76413793650a49f8b1ad3e4d9805b
Lixun Zhang <lixun.zhang@amd.com>
2022-07-15 16:02:31 +0000

    Automated the process of switch test dir

--- inst.sh
+++ inst.sh
@@ -1,6 +1,18 @@
 #! /bin/bash
 
-cd ~/llvm-project-mlir/build/
-cmake -G Ninja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_FAT_LIBMLIRMIOPEN=1 ../
-ninja libMLIRMIOpen
-cmake --install . --component libMLIRMIOpen --prefix ~/dummy
+##
+## Switch between test_debug and test_orig
+## $1: debug or orig
+##     Only use test_debug if $1 is explicitly set to debug
+##     All other cases will use the orig test
+
+cd ~/llvm-project-mlir/mlir
+if [[ $1 == "debug" ]];then
+    echo "switch to test_debug"
+    rm -r test
+    cp -r test_debug test
+else
+    echo "switch to test_orig"
+    rm -r test
+    cp -r test_orig test
+fi

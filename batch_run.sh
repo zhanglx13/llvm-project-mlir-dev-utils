8b672d2c553c5f29a4dac79d274e6617985e691a
Lixun Zhang <lixun.zhang@amd.com>
2022-07-15 16:03:30 +0000

    Added 0.001f to the threshold
    Note that we should no longer need the configs.sh

--- batch_run.sh
+++ batch_run.sh
@@ -9,7 +9,7 @@ result_filename=$2
 for config in 1 2 3 4
 do
     echo "Running config $config ..." | tee -a ${result_filename}
-    for thr in 0.25 0.2 0.15 0.1 0.05 0.01
+    for thr in 0.25 0.2 0.15 0.1 0.05 0.01 0.001
     do
         for i in  $(seq 0 1 9)
         do

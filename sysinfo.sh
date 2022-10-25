c1c6742ad8abeb572ecf807e9567a95ac7b3b0c3
Lixun Zhang <lixun.zhang@amd.com>
2022-10-25 12:29:16 -0500

    add more info to sysinfo

--- sysinfo.sh
+++ sysinfo.sh
@@ -3,24 +3,32 @@
 UBUNTU_VERSION=$(lsb_release -a | sed -n '/Description/p' | awk '{print $3}')
 LINUX_KERNEL=$(uname -r)
 LINUX_KERNEL=${LINUX_KERNEL%%-generic}
-ROCM_VERSION=$(ls -al /opt | sed -n '/rocm-/p' | tail -n 1 | awk '{print $(NF)}')
-ROCM_VERSION=${ROCM_VERSION##rocm-}
+ROCM_VERSION=$(dpkg -l | grep "rocm-dev " | awk '{print $3}')
 CPU_NAME=$(cat /proc/cpuinfo | grep "model name" | head -n 1 | awk -F: '{print $2}')
 NUM_PROCESSORS=$(cat /proc/cpuinfo | grep "siblings" | head -n 1 | awk -F: '{print $2}')
 CPU_NAME=${CPU_NAME:1}
 NUM_PROCESSORS=${NUM_PROCESSORS:1}
 VBIOS=$(rocm-smi -v | grep "VBIOS version:"| head -n 1)
 VBIOS=${VBIOS##*version: }
+GPU_ARCH=$(rocm_agent_enumerator | tail -n 1)
 NUM_GPUS=$(rocm_agent_enumerator | wc -l)
-#NUM_GPUS=$NUM_GPUS-1
 ((NUM_GPUS--))
 MB=$(sudo dmidecode -s baseboard-product-name)
+BIOS_VENDOR=$(sudo dmidecode -s bios-vendor)
+BIOS_VERSION=$(sudo dmidecode -s bios-version)
+RELEASE_DATE=$(sudo dmidecode -s bios-release-date)
+HD_CLASS=$(hwinfo --disk | grep "Hardware Class:" | awk '{print $3}')
+HD_DEVICE=$(hwinfo --disk | grep "Device:" | awk '{print $2}')
+HD_DEVICE=${HD_DEVICE#\"}
+HD_DEVICE=${HD_DEVICE%\"}
 
 echo "CPU:            ${CPU_NAME}"
 echo "processors:     ${NUM_PROCESSORS}"
 echo "Ubuntu version: ${UBUNTU_VERSION}"
 echo "Linux kernel:   ${LINUX_KERNEL}"
 echo "ROCm version:   ${ROCM_VERSION}"
-echo "Number of GPUs: ${NUM_GPUS}"
+echo "GPU:            ${GPU_ARCH} x ${NUM_GPUS}"
 echo "GPU vbios:      ${VBIOS}"
 echo "Motherboard:    ${MB}"
+echo "BIOS:           ${BIOS_VENDOR} ${BIOS_VERSION} (${RELEASE_DATE})"
+echo "HD:             ${HD_CLASS} (${HD_DEVICE})"

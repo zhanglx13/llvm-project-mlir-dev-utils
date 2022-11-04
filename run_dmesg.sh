4d5df0e11ee68da3f150f828f2de52ba8a88589f
Lixun Zhang <zhanglx@utexas.edu>
2022-11-03 22:14:52 -0500

    add

--- run_dmesg.sh
+++ run_dmesg.sh
@@ -6,9 +6,12 @@ CONTAINER=zhanglx-mlir-dev
 CMD="ninja check-rocmlir"
 RESULT_DIR=nightly_random
 
-## Clear the dmesg buffer
-sudo dmesg -C
-## Execute the command in the container
-docker exec --workdir ${WORK_DIR} ${CONTAINER} ${CMD} | ts '[%H:%M:%S]' | tee -a ~/${RESULT_DIR}/lit_result$1.txt
-## Obtain the dmesg
-dmesg --kernel --ctime --userspace --decode > ~/${RESULT_DIR}/dmesg_log$1.txt
+for n in a b
+do
+    ## Clear the dmesg buffer
+    sudo dmesg -C
+    ## Execute the command in the container
+    docker exec --workdir ${WORK_DIR} ${CONTAINER} ${CMD} | ts '[%H:%M:%S]' | tee -a ~/${RESULT_DIR}/lit_result$n.txt
+    ## Obtain the dmesg
+    dmesg --kernel --ctime --userspace --decode > ~/${RESULT_DIR}/dmesg_log$n.txt
+done

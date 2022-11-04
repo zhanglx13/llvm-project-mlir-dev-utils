b8f7aaf9c3dbb12e7afa0ac9e182d3c3bfb62d07
Lixun Zhang <zhanglx@utexas.edu>
2022-11-03 22:27:11 -0500

    Restart the container at every run

--- run_dmesg.sh
+++ run_dmesg.sh
@@ -8,10 +8,18 @@ RESULT_DIR=nightly_random
 
 for n in a b
 do
+    echo "-------------------------------------------------"
+    echo "Start run $n"
+    echo "-------------------------------------------------"
     ## Clear the dmesg buffer
     sudo dmesg -C
+    ## Start the container
+    docker container start ${CONTAINER}
     ## Execute the command in the container
     docker exec --workdir ${WORK_DIR} ${CONTAINER} ${CMD} | ts '[%H:%M:%S]' | tee -a ~/${RESULT_DIR}/lit_result$n.txt
     ## Obtain the dmesg
     dmesg --kernel --ctime --userspace --decode > ~/${RESULT_DIR}/dmesg_log$n.txt
+    ## Stop the container
+    docker container stop ${CONTAINER}
+    sleep 5
 done

43f3f060f2eddb50a1a6ee60fffdae8fc2a2e928
Lixun Zhang <lixun.zhang@amd.com>
2022-11-04 11:48:04 -0500

    Added cmake config and build command

--- run_dmesg.sh
+++ run_dmesg.sh
@@ -1,16 +1,33 @@
 #! /bin/bash
 
-## $1: run index
+## $1: testsuite: fixed or random
 WORK_DIR=/home/zhanglx/rocMLIR/build
 CONTAINER=zhanglx-mlir-dev
 CMD="ninja check-rocmlir"
-RESULT_DIR=nightly_random
+RESULT_DIR=nightly_$1
+
+echo "Start container"
+docker container start ${CONTAINER}
+## cmake configure and build for fixed nightly test
+if [[ "$1" == "fixed" ]]; then
+    echo "Config and build for nightly fixed tests"
+    docker exec --workdir ${WORK_DIR} ${CONTAINER} cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -DROCMLIR_DRIVER_ENABLED=1 -DROCMLIR_DRIVER_PR_E2E_TEST_ENABLED=0 -DROCMLIR_DRIVER_E2E_TEST_ENABLED=1 -DROCK_E2E_TEST_ENABLED=1 -DROCMLIR_DRIVER_MISC_E2E_TEST_ENABLED=1 -DROCMLIR_DRIVER_TEST_GPU_VALIDATION=1 "-DLLVM_LIT_ARGS=-v --time-tests" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+fi
+
+## cmake configure and build for random nightly test
+if [[ "$1" == "random" ]]; then
+    echo "Config and build for nightly random tests"
+    docker exec --workdir ${WORK_DIR} ${CONTAINER} cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo -DROCMLIR_DRIVER_ENABLED=1 -DROCMLIR_DRIVER_PR_E2E_TEST_ENABLED=0 -DROCMLIR_DRIVER_E2E_TEST_ENABLED=1 -DROCK_E2E_TEST_ENABLED=1 -DROCMLIR_DRIVER_RANDOM_DATA_SEED=1 -DROCMLIR_DRIVER_MISC_E2E_TEST_ENABLED=0 -DROCMLIR_DRIVER_TEST_GPU_VALIDATION=0 "-DLLVM_LIT_ARGS=-v --time-tests" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
+fi
+## Stop the container
+echo "Stop container"
+docker container stop ${CONTAINER}
 
 for n in a b
 do
-    echo "-------------------------------------------------"
+    echo "-----------------------------------------------------------"
     echo "Start run $n"
-    echo "-------------------------------------------------"
+    echo "-----------------------------------------------------------"
     ## Clear the dmesg buffer
     dmesg -C
     ## Start the container

b82c66a955547022fc6cd603995252bc3cbc55da
Lixun Zhang <zhanglx@utexas.edu>
2022-11-03 22:08:06 -0500

    fix

--- run_dmesg.sh
+++ run_dmesg.sh
@@ -3,14 +3,12 @@
 ## $1: run index
 WORK_DIR=/home/zhanglx/rocMLIR/build
 CONTAINER=zhanglx-mlir-dev
-CMD="ninja check-rocmlir | ts '[%H:%M:%S]' | tee -a lit_result$1.txt"
+CMD="ninja check-rocmlir"
 RESULT_DIR=nightly_random
 
 ## Clear the dmesg buffer
 sudo dmesg -C
 ## Execute the command in the container
-docker exec --workdir ${WORK_DIR} ${CONTAINER} ${CMD}
+docker exec --workdir ${WORK_DIR} ${CONTAINER} ${CMD} | ts '[%H:%M:%S]' | tee -a ~/${RESULT_DIR}/lit_result$1.txt
 ## Obtain the dmesg
 dmesg --kernel --ctime --userspace --decode > ~/${RESULT_DIR}/dmesg_log$1.txt
-## Copy the result
-docker exec --workdir ${WORK_DIR} ${CONTAINER} cp lit_result$1.txt /data/${RESULT_DIR}/

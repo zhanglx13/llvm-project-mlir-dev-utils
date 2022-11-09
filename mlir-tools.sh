73b7badefe4903098ff73490bb6971d0910abecf
Lixun Zhang <lixun.zhang@amd.com>
2022-11-09 10:49:19 -0600

    Fixed mlir-cpu-runner

--- mlir-tools.sh
+++ mlir-tools.sh
@@ -9,7 +9,7 @@ LIB_CONV_VALID=${BUILD_DIR}/lib/libconv-validation-wrappers.so
 LIB_MLIR_RUNNER_UTILS=${BUILD_DIR}/external/llvm-project/llvm/lib/libmlir_runner_utils.so
 ROCMLIR_DRIVER="${BIN_DIR}/rocmlir-driver"
 MLIR_ROCM_RUNNER="${BIN_DIR}/mlir-rocm-runner --shared-libs=${LIB_MLIR_ROCM_RUNTIME},${LIB_CONV_VALID},${LIB_MLIR_RUNNER_UTILS} --entry-point-result=void"
-MLIR_CPU_RUNNER="${UP_BIN_DIR}/mlir-cpu-runner --shared-libs=${LIB_MLIR_ROCM_RUNTIME},${LIB_MLIR_RUNNER_UTILS} --entry-point-result=void"
+MLIR_CPU_RUNNER="${UP_BIN_DIR}/mlir-cpu-runner --shared-libs=${LIB_MLIR_ROCM_RUNTIME},${LIB_CONV_VALID},${LIB_MLIR_RUNNER_UTILS} --entry-point-result=void"
 ROCMLIR_GEN="${BIN_DIR}/rocmlir-gen"
 ROCMLIR_OPT="${BIN_DIR}/rocmlir-opt"
 MLIR_OPT="${UP_BIN_DIR}/mlir-opt"
--- navi21_multithreading/run_config.sh
+++ navi21_multithreading/run_config.sh
@@ -6,4 +6,4 @@ source ../mlir-tools.sh
 CONFIG="-batchsize=64 -in_channels=512 -in_h=7 -in_w=7 -out_channels=512 -fil_h=3 -fil_w=3 --dilation_h=1 --dilation_w=1 --conv_stride_h=1 --conv_stride_w=1 --padding_h=1 --padding_w=1 --operation conv2d_bwd_data -fil_layout=gkyxc -in_layout=nhwgc -out_layout=nhwgk -t f32  --arch gfx1030 -pv_with_gpu"
 
 printf "Instance %02d:"  $1
-${ROCMLIR_GEN} ${CONFIG} | ${ROCMLIR_DRIVER} -c | ${MLIR_ROCM_RUNNER}
+${ROCMLIR_GEN} ${CONFIG} | ${ROCMLIR_DRIVER} -c | ${MLIR_CPU_RUNNER}

a9c4b7cdb7756116059d06f8d155c4b355f3421c
root <root@smc300x-clt-r4c6-18.cs-clt.dcgpu>
2024-06-04 02:06:56 +0000

    Update container bring script

--- container_bringup.sh
+++ container_bringup.sh
@@ -10,11 +10,11 @@ if [[ $# -lt 1 ]]; then
     exit
 fi
 
-cd ~/
+
 
 # setup emacs
 git clone https://$1@github.com/zhanglx13/emacs_settings.git
-mv emacs_settings .emacs.d
+mv emacs_settings ~/.emacs.d
 
 # setup myscrips
 git clone https://$1@github.com/zhanglx13/llvm-project-mlir-dev-utils.git
@@ -24,17 +24,14 @@ mv llvm-project-mlir-dev-utils myscripts
 git clone https://$1@github.com/zhanglx13/orgfiles.git
 
 # Download rocMLIR
-git clone https://$1@github.com/ROCmSoftwarePlatform/rocMLIR.git
+git clone https://$1@github.com/ROCm/rocMLIR.git
 
 # Download MIOpen
 # git clone https://$1@github.com/ROCmSoftwarePlatform/MIOpen.git
 
 # Download triton
-git clone https://$1@github.com/ROCmSoftwarePlatform/triton.git
-cd triton
-git co dot-rocMLIR
-cd ..
+git clone https://$1@github.com/zhanglx13/triton.git AMD-triton
+git clone https://$1@github.com/triton-lang/triton.git OAI-triton
 
 # Download msft
 git clone https://$1@github.com/ROCmSoftwarePlatform/msft_amd_ai_operators.git
-

40ea07290a6daaecfb1bf5107ef7c97202d6d525
Lixun Zhang <lixun.zhang@amd.com>
2025-10-08 20:46:30 +0000

    Add git command

--- container_bringup.sh
+++ container_bringup.sh
@@ -27,3 +27,11 @@ git clone https://github.com/llvm/llvm-project.git
 # Download triton
 git clone https://$1@github.com/triton-lang/triton.git OAI-triton
 git clone https://$1@github.com/ROCm/triton.git AMD-triton
+
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global user.name "Lixun Zhang"
+git config --global user.email "lixun.zhang@amd.com"
